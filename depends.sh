5a39576a6c0a225b0dfed55e0e05b77073cf3447
Atmgg <stilldiao@126.com>
2014-05-27 05:16:40 -0400

    mod depends script

--- depends.sh
+++ depends.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #this project depends on blade,log4cpp
+# the "=" can't have blank bwtween it 
 current_dir=$(cd $(dirname $0) && pwd)
 cd /tmp
+# if must have blank beside "[" and "]"
 if [ ! -d "log4cpp-codegit" ]; then
 	git clone http://git.code.sf.net/p/log4cpp/codegit log4cpp-codegit
 fi
