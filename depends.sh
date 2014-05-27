e50eb68b8f206b2febafddd7cd2ab7c1f614bf4f
Atmgg <stilldiao@126.com>
2014-05-27 01:30:53 -0400

    add a script to install third-party libs

---
+++ depends.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#this project depends on blade,log4cpp
+current_dir=$(cd $(dirname $0) && pwd)
+cd /tmp
+if [ ! -d "log4cpp-codegit" ]; then
+	git clone http://git.code.sf.net/p/log4cpp/codegit log4cpp-codegit
+fi
+cd log4cpp-codegit
+./autogen.sh
+./configure
+make
+make check
+sudo make install
+
+echo -n "install log4cpp succeed."
+cd $current_dir
